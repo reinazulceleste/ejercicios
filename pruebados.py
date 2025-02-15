d27f3fc5a5e9fee88d4f5b305ee8bcad0a3aa69e
reinazulceleste <dianammj79@gmail.com>
2025-02-15 20:32:44 +0100

    ve1

---
+++ pruebados.py
@@ -0,0 +1,7 @@
+def rev_sentence(sentence):
+    word= sentence.split('')
+    reverse_sentence= ' '.join(reversed(words))
+    return reverse_sentence #hola commit
+if words== "main":
+    input= "codigo de practica de prueba de geeks"
+print(rev_sentence(input))
